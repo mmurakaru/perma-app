e3bc3da173ab4bef5a4bc972e67dc8c5c8689cfd
mmurakaru <neuefische@Markus-MacBook-Pro-2.local>
2020-11-27 15:49:18 +0100

    Cleanup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+const express = require("express")
+const jwt = require("jsonwebtoken")
 const cors = require("cors")
-const fetch = require('node-fetch');
-const myLogger = require('./middleware/myLogger')
+const fetch = require('node-fetch')
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors())
-
-app.use(myLogger())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -19,12 +16,9 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-
-
-
 // The parameters for our POST request
 const params = {
-  origin: 'http://localhost:3001/',
+  origin: 'http://localhost:3000',
   token: '5slwOv-vNendNI-A4LtwJJiM0KHc_t-nTx4zxp88yMI'
 }
 
--- server/src/middleware/myLogger.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  return (req, res, next) => {
-    console.log(req.method, req.body, req.url)
-    next()
-  }
-}
-
