94f067ca661471fee478220a5eebdc3cd65e0e50
mmurakaru <neuefische@Markus-MacBook-Pro-2.local>
2020-11-27 18:23:04 +0100

    Add Express.js token request logic

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3001;
 
 app.use(cors())
 
+app.use('/api/token', async (req, res) => {
+  const token = await getJWT()
+  res.send(token)
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
@@ -17,18 +22,19 @@ app.listen(port, () => {
 });
 
 // The parameters for our POST request
+function getJWT() {
 const params = {
   origin: 'http://localhost:3000',
   token: '5slwOv-vNendNI-A4LtwJJiM0KHc_t-nTx4zxp88yMI'
 }
 
-fetch('https://trefle.io/api/auth/claim', {
+return fetch('https://trefle.io/api/auth/claim', {
   method: 'post',
   body: JSON.stringify(params),
   headers: { 'Content-Type': 'application/json' }
 })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  })
+  .then( response => response.json())
+  .then(data => data)
   .catch(err => console.log(err))
+}
+
