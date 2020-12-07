d7a745e92b24c452239e708d2a8cf264fbd15aa5
mmurakaru <neuefische@Markus-MacBook-Pro-2.local>
2020-12-07 11:20:50 +0100

    Add styleguidist

--- client/src/App.js
+++ client/src/App.js
@@ -76,7 +76,6 @@ function App() {
                             handleClick={showPlantDetails}
                         />
                     </PageContainer>
-                    <Navigation />
                 </Route>
                 <Route path="/PlantFields">
                     <PlantFields
